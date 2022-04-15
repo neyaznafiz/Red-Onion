55f567ac547adebaa2f04d1a0354c869764d735e
niiyazobug <niiyaz5555@gmail.com>
2022-04-16 00:10:31 +0600

    delete socialsignin method from login and call socialSignin component

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo2 from '../../../Images/logo2.png'
-import { AiFillGoogleCircle } from 'react-icons/ai'
-import { AiFillTwitterCircle } from 'react-icons/ai'
-import { BsFacebook } from 'react-icons/bs'
-import useSocialHooks from '../../Shared/SocialHooks/useSocialHooks';
+import SocialSignin from '../SocialSignin/SocialSignin';
 
 const Login = () => {
-
-    const {signInWithGoogle, signInWithFacebook, signInWithTwitter}=useSocialHooks()
 
     return (
         <div>
@@ -37,20 +32,7 @@ const Login = () => {
 
                     <div className='justify-around py-44 px-16 w-2/6'>
 
-                        <div>
-                            <button onClick={()=> signInWithGoogle()} className=' rounded-full flex pl-6 pr-5 py-2 text-white bg-red-500'> <AiFillGoogleCircle className='mt-1 mr-2'></AiFillGoogleCircle> Continue witth google</button>
-                        </div>
-
-                        <div className='my-3'>
-                            <button onClick={()=>signInWithTwitter()} className=' rounded-full flex pl-6 pr-5 pr-9 py-2 text-white bg-red-500'> <AiFillTwitterCircle className='mt-1 mr-2'></AiFillTwitterCircle> Continue witth Twitter</button>
-                        </div>
-
-                        <div>
-                            <button onClick={() => signInWithFacebook()} className=' rounded-full flex pl-6 pr-9 py-2 text-white bg-red-500'> <BsFacebook className='mt-1 mr-2'></BsFacebook> Continue witth Facebook</button>
-                        </div>
-
-
-
+                        <SocialSignin></SocialSignin>
                     </div>
                 </div>
 
