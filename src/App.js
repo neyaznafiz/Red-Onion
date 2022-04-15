29bba148b109dfc5929c80dcaa3f12c4017eaec9
niiyazobug <niiyaz5555@gmail.com>
2022-04-15 14:10:40 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Pages/Home/Home';
+import Cart from './Components/Pages/Cart/Cart';
+import Login from './Components/Pages/Login/Login';
+import SignUp from './Components/Pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className="">
       <Header></Header>
+
+      <Routes>
+
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/cart' element={<Cart></Cart>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+
+      </Routes>
     </div>
   );
 }
--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo2 from '../../../Images/logo2.png'
+import {FiShoppingCart} from 'react-icons/fi'
 
 const Header = () => {
     return (
         <nav className='border flex justify-between bg-gray-50 px-16 py-4'>
+
             <div>
+            <Link to="/">
             <img className='w-32' src={logo2} alt="" />
+            </Link>
             </div>
 
             <div className='flex gap-9'>
-            <Link className='font-semibold pt-1' to="/login">Login</Link>
-            <Link className='bg-red-500 text-white rounded-full border px-5 pt-1' to="/Signup">Sign Up</Link>
+
+                <Link to='/cart' className='pt-2  '><FiShoppingCart></FiShoppingCart></Link>
+
+            <Link to="/login" className='font-semibold pt-1'>Login</Link>
+
+            <Link to="/signup" className='bg-red-500 text-white rounded-full px-5 pt-1'>Sign Up</Link>
+
             </div>
+
         </nav>
     );
 };
