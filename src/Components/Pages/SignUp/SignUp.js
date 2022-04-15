80dd8df73a489b9bbcbb1ed57cd552261ff55dc7
niiyazobug <niiyaz5555@gmail.com>
2022-04-15 16:46:41 +0600

    img added in signup

--- src/Components/Pages/SignUp/SignUp.js
+++ src/Components/Pages/SignUp/SignUp.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from 'react'
+import  logo2  from '../../../Images/logo2.png'
 
 const SignUp = () => {
     return (
         <div>
-            signup is this
+
+            <div>
+
+            <div className='flex justify-center'>
+                <img className='w-64' src={logo2} alt="" />
+            </div>
+
+            </div>
+            
         </div>
     );
 };
